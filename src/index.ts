8a67c5293cdf08f7f76cc4ca7803275d31961be4
Ryan Anstotz <ryananstotz@gmail.com>
2020-12-15 20:26:51 -0500

    update tsconfig lib version. Streamline observables and data collection strategy.

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,27 @@
 import Parser from 'rss-parser';
-import { forkJoin } from 'rxjs';
+import { forkJoin, of, from } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-
-
-const targetFeeds = [
-  // contentSnippet appears to contain description
-  'http://rss.cnn.com/rss/cnn_topstories.rss',
-  'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-  'http://feeds.washingtonpost.com/rss/national',
-  'https://feeds.npr.org/1001/rss.xml',
-  'http://feeds.foxnews.com/foxnews/national',
-  'https://www.oann.com/category/newsroom/feed/',
-]
 const parser = new Parser();
-const feedArray = targetFeeds.map((feed) => {
-  return parser.parseURL(feed);
-})
 
-console.log(feedArray)
+// use from to transform the promise returned by parser into an observable
+const targetFeeds = {
+  cnn: from(parser.parseURL('http://rss.cnn.com/rss/cnn_topstories.rss')).pipe(catchError(error => of(error))),
+  nytimes: from(parser.parseURL('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml')).pipe(catchError(error => of(error))),
+  wapo: from(parser.parseURL('http://feeds.washingtonpost.com/rss/national')).pipe(catchError(error => of(error))),
+  npm: from(parser.parseURL('https://feeds.npr.org/1001/rss.xml')).pipe(catchError(error => of(error))),
+  foxnews: from(parser.parseURL('http://feeds.foxnews.com/foxnews/national')).pipe(catchError(error => of(error))),
+  oann: from(parser.parseURL('https://www.oann.com/category/newsroom/feed/')).pipe(catchError(error => of(error))),
+  // error scenario for bad url
+  // oann: from(parser.parseURL('https://www.oasdffdnn.com/csfdsaflkjategory/newsroom/feed/')).pipe(catchError(error => of(error))),
+}
 
-const observable = forkJoin(feedArray);
-observable.subscribe((resArray) => {
-  resArray.map((item) => {
-    console.log(item.title)
-  })
-})
-
-// the observable pattern below works well
-// implement this with RxJS style pipes to collect all necessary data. 
-
-// const firstUrl = 'http://rss.cnn.com/rss/cnn_topstories.rss';
-// const secondUrl = 'http://feeds.foxnews.com/foxnews/national';
-// const observable = forkJoin([parser.parseURL(firstUrl), parser.parseURL(secondUrl)]);
-// observable.subscribe(([resA, resB]) => {
-//   console.log(resA.title)
-//   console.log(resB.title)
-// })
+// TODO:
+// use pipe on forkjoin, then filter the data we want into a format that can be sent to firestore
+// potentially store any error data elsewhere (or filter it entirely, or just say 'error')
+const observable = forkJoin(targetFeeds);
+observable.subscribe(console.log);
 
 
-
-// rss-parser types example
-// type CustomFeed = { foo: string };
-// type CustomItem = { bar: number };
-// // example of using types to ensure accurate data
-// const parser: Parser<CustomFeed, CustomItem> = new Parser({
-//   customFields: {
-//     feed: ['foo', 'baz'],
-//     //            ^ will error because `baz` is not a key of CustomFeed
-//     item: ['bar']
-//   }
-// });
-
-
+// lesson: https://www.learnrxjs.io/learn-rxjs/operators/transformation/map#signature-map-project-function-thisarg-any-observable
+// forkjoin: https://www.learnrxjs.io/learn-rxjs/operators/combination/forkjoin

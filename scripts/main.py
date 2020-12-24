a54a1b7f11d2ca14bbf81ef8a43b1215db27fe00
Ryan Anstotz <ryananstotz@gmail.com>
2020-12-23 22:12:03 -0500

    update script to get all rss data from required fields. Place data in appropriate prod-ready data structure to be stored in an object store later. Improve data cleaning.

--- scripts/main.py
+++ scripts/main.py
@@ -4,12 +4,6 @@ import re
 
 pp = pprint.PrettyPrinter(indent=4)
 
-
-def sanitize_input(input: str) -> str:
-    ''' Removes html tags and strips whitespace chars (' ', \n, \r, tab). '''
-    return re.sub('<[^<]+?>', '', input).strip()
-
-
 feeds = [
     {'name': 'cnn', 'url': 'http://rss.cnn.com/rss/cnn_topstories.rss'},
     {'name': 'nytimes', 'url': 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'},
@@ -21,10 +15,19 @@ feeds = [
     # { name: 'oann', url: 'https://www.oasdffdnn.com/csfdsaflkjategory/newsroom/feed/' },
 ]
 
-max_articles = 3
+max_articles = 5
 
 # not all feeds provide these, need to account for that
 target_keys = ['author', 'link', 'published', 'title', 'summary']
+
+
+def sanitize_input(input: str) -> str:
+    ''' Removes html tags and strips whitespace chars (' ', \n, \r, tab). '''
+    res = re.sub('<[^<]+?>', '', input).strip()
+    res = res.replace(u'\xa0', u' ')
+    res = res.replace('\n', '')
+    res = res.replace('\r', '')
+    return res
 
 
 def get_articles(target_rss_feed: str, max_articles: int) -> list:
@@ -36,27 +39,40 @@ def get_articles(target_rss_feed: str, max_articles: int) -> list:
     return articles
 
 
-returned_articles = get_articles(feeds[0]['url'], max_articles)
+articles = get_articles(feeds[1]['url'], max_articles)
 
 
 def filter_article(article: dict, filter_fields: list) -> dict:
     ''' takes full article and filters the fields based on filter_field list provided. '''
-    def field_getter(_article: dict, _field: str) -> str:
-        ''' return empty field if dict key doesn't exist. '''
+    def field_sanitizer(_article: dict, _field: str) -> str:
+        ''' sanitizes field and returns empty field if dict key doesn't exist. '''
         res = ''
         try:
-            res = _article[_field]
+            res = sanitize_input(_article[_field])
         except:
             res = ''
         return res
 
-    filtered_dict = {field: field_getter(
+    filtered_dict = {field: field_sanitizer(
         article, field) for field in filter_fields}
     return filtered_dict
 
 
 # gather all returned articles and filter them
 filtered_articles = [filter_article(article, target_keys)
-                     for article in returned_articles]
+                     for article in articles]
 
 pp.pprint(filtered_articles)
+
+feed_data = []
+for item in feeds:
+    articles = get_articles(item['url'], max_articles)
+    res = {
+        item['name']: [filter_article(article, target_keys)
+                       for article in articles]
+    }
+    feed_data.append(res)
+
+pp.pprint(feed_data)
+# do something with feed_data
+# restructure script to be more friendly
